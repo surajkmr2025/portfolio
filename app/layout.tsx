3e60eaeb2ede59890b60872ca763573ef333467e
suraj kumar <surajkumar44727@gmail.com>
2026-06-10 21:15:25 +0000

    add manual aceternity ui configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Sidebar from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 
 const geistSans = Geist({

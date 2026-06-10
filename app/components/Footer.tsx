8516b360c40d885623f078df2799a1b950c91d34
suraj kumar <surajkumar44727@gmail.com>
2026-06-10 21:10:40 +0000

    add Footer and fix some styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,36 @@
-export default function Footer() {
+"use client"
+
+import { relative } from "path";
+import React, { useState, useEffect } from "react";
+import { TextHoverEffect } from "@/app/components/ui/text-hover-effect";
+
+
+const formatTime = () => new Date().toLocaleTimeString();
+
+export default function Footer(): React.ReactElement {
+
+  const [timeNow, setTimeNow] = useState<string>("");
+
+  useEffect(() => {
+    const initialId = window.setTimeout(() => setTimeNow(formatTime()))
+    const intervalId = window.setInterval(() => setTimeNow(formatTime()), 1000);
+    return () => {
+      window.clearTimeout(initialId);
+      window.clearInterval(intervalId);
+    }
+  }, []);
+
   return (
-    <main>
-      
-    </main>
+    <footer className="relative mx-auto pb-12 md:pb-0 lg:pb-0 xl:pb-0 max-w-303-zinc-950 border-t border-zinc-800 text-zinc-300 overflow-hidden">
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4 px-20 py-5">
+        <div className="text-lg cursor-pointer">
+          Reach out →
+        </div> 
+              <TextHoverEffect text="SURAJ" />
+        <div className="text-lg">
+          {timeNow}
+        </div>
+      </div>
+    </footer>
   );
 }

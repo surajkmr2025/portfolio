94085639f52c778edf1cd64ba6949b6e4214be50
suraj kumar <surajkumar44727@gmail.com>
2026-06-27 10:03:18 +0000

    add resume

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -49,6 +49,12 @@ const CloseIcon = ({ className, ...props }: IconProps) => (
   </svg>
 );
 
+const ResumeIcon = ({ className, ...props }: IconProps) => (
+   <svg viewBox="0 0 24 24" fill="none" strokeWidth={2} stroke="currentColor" aria-hidden="true" className={className} {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+  </svg>
+)
+
 // const ExperienceIcon = ({ className, ...props }: IconProps) => (
 //   <svg viewBox="0 0 24 24" fill="none" strokeWidth={2} stroke="currentColor" aria-hidden="true" className={className} {...props}>
 //     <path strokeLinecap="round" strokeLinejoin="round" d="M7 3h10l3 4v14H4V7z" />
@@ -91,13 +97,14 @@ export default function Sidebar(): React.ReactElement {
   }, [isMobileMenuOpen]);
 
 
-  const navItems: { href: string; label: string; Icon: React.ComponentType<IconProps> }[] = [
+  const navItems: { href: string; label: string; Icon: React.ComponentType<IconProps>; isExternal?:boolean}[] = [
     { href: "/", label: "Home", Icon: HomeIcon },
     { href: "/projects", label: "Projects", Icon: ProjectsIcon },
     // { href: "/experience", label: "Experience", Icon: ExperienceIcon },
     { href: "/tools", label: "Tools", Icon: ToolsIcon },
     { href: "/about", label: "About", Icon: UserIcon },
     { href: "/contact", label: "Contact", Icon: MailIcon },
+    { href: '/Suraj.pdf', label: "Resume", Icon: ResumeIcon, isExternal: true }
   ];
 
   const socials = [
